a9813b424eb5d9ca7b5513d2474ea3e056af62b8
renfie <102348785+renfie@users.noreply.github.com>
2024-03-19 09:16:07 +0100

    Update unbound-update.sh

--- unbound-update.sh
+++ unbound-update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-curl --output /home/pi/docker-files/unbound-n/data/root.hints https://www.internic.net/domain/named.root
-curl --output /home/pi/docker-files/unbound-n/data/auth-zone/root.zone https://www.internic.net/domain/root.zone
+curl --output ${PWD}/data/root.hints https://www.internic.net/domain/named.root
+curl --output ${PWD}/data/auth-zone/root.zone https://www.internic.net/domain/root.zone
 docker restart unbound-n
-#synowebapi --exec api=SYNO.Docker.Container version=1 method=restart name=unbound
